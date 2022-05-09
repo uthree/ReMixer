62cbd5ef2d03807c4c6c322f3b615ac5cbc735c6
UThree <33905956+uthree@users.noreply.github.com>
2022-05-09 16:20:40 +0900

    Update remixer.py

--- remixer.py
+++ remixer.py
@@ -282,7 +282,7 @@ class SwinReS2MLP(nn.Module):
         modules.append(nn.Conv2d(channels[-1], output_channels, 1, 1, 0) if output_channels else nn.Identity())
         modules.append(
                 nn.Sequential(
-                    nn.AvgPool2d(2**(len(stages))),
+                    lambda x: torch.sum(x, [2, 3]),
                     nn.Flatten(),
                     nn.Linear(channels[-1], output_dim) # if given output_dim, added pooling and fully-connected finaly.
                     ) if output_dim else nn.Identity()) 
